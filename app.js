af3130065e10d7c6164db8d6cdadf72abd890cbf
reymesson <reymesson@gmail.com>
2017-06-05 07:32:27 -0400

    Update app.js

--- app.js
+++ app.js
@@ -4,13 +4,9 @@ net.createServer(function (socket) {
 
 	console.log('Socket created.');
 	socket.on('data', function(data) {
+	
+		socket.write("HTTP/1.1 200 OK\n\n");
+		socket.end();
+	})
 	  
-	  console.log('RESPONSE: ' + data);
-	}).on('connect', function() {
-	  
-	  socket.write("GET / HTTP/1.0\r\n\r\n");
-	}).on('end', function() {
-	  console.log('DONE');
-	});
-
-}).listen(8080);
+}).listen(6719);
