ebc13fcadbce1f19ac43772e4da6dc7cc53b2e48
Rey E Messon Piña <20066719@academico.pucmm>
2017-07-05 17:13:19 -0400

    Agregamos el arbol

--- webPaa515/public/js/custom.js
+++ webPaa515/public/js/custom.js
@@ -1,7 +1,96 @@
-var attr = "font-size: 2.25em;color: black;font-size: 2.25em;margin-bottom: .35em;padding-bottom: .15em;border-bottom: 1px solid #0cf;text-shadow: 1px 2px 1px rgba(0,0,0,.5);";
+var jsonObj = [{
+	"name": "html",
+	"parent": "null",
+	"children": [
+		{
+			"name": "head",
+			"parent": "html"
+		},
+		{
+			"name": "body",
+			"parent": "html",
+			"children": [
+				{
+					"name": "h1",
+					"parent": "body"
+				},
+				{
+					"name": "div",
+					"parent": "body",
+					"children": [
+						{
+							"name": "p",
+							"parent": "body",
+							"children": [
+								{
+									"name": "strong",
+									"parent": "p"
+								}
+							]
+						}
+					]
+				},
+				{
+					"name": "div",
+					"parent": "body",
+					"children": [
+						{
+							"name": "h2",
+							"parent": "div"
+						},
+						{
+							"name": "p",
+							"parent": "div"
+						},
+						{
+							"name": "ul",
+							"parent": "div",
+							"children": [
+								{
+									"name": "li",
+									"parent": "ul"
+								},
+								{
+									"name": "li",
+									"parent": "ul"
+								}
+							]
+						}
+					]
+				}
+			]
+		}
+	]
+}];
 
-document.getElementById("page-title").setAttribute("style", attr);
+var listEl = document.getElementById("tree");
 
-var attr2 = "position: relative;padding: 25px 25px 10px 52%;min-height: 225px;border: 2px solid #00258e;background: rgba(255,255,255,.5) url(../images/new-burst.png) 108% 116% no-repeat;box-shadow: inset 0 0 5px rgba(0,0,0,0.25);border-radius: 0 0 10px 10px;z-index: 1;padding: .25em 25px;margin: 0 0 -.25em;color: #fc0;text-shadow: 2px 2px 0 rgba(0,0,0,0.5);border-bottom: 2px solid #001a64;border-radius: 10px 10px 0 0;background-color: #00258e;background-image: -moz-linear-gradient(#123bad,#00258e);background-image: -ms-linear-gradient(#123bad,#00258e);background-image: -o-linear-gradient(#123bad,#00258e);background-image: -webkit-linear-gradient(#123bad,#00258e);background-image: linear-gradient(#123bad,#00258e);";
+makeList(jsonObj,listEl);
 
-document.getElementById('new-item').setAttribute("style",attr2);
+function makeList( jsonObject, listElement){
+    for(var i in jsonObject){//iterate through the array or object
+        //insert the next child into the list as a <li>
+        var newLI = document.createElement('li');
+
+
+
+        if  (jsonObject[i] instanceof Array){
+            newLI.innerHTML=i+": ARRAY";            
+        }
+        else if (  jsonObject[i] instanceof Object){
+            newLI.innerHTML=i+": OBJECT";            
+        }
+        else
+            newLI.innerHTML=i+': '+jsonObject[i];
+        listElement.appendChild(newLI);   
+        //insert a <ul> for nested nodes 
+        if  (jsonObject[i] instanceof Array || jsonObject[i] instanceof Object){
+            var newUL = document.createElement('ul');
+            //newUL.innerHTML=i;
+            listElement.appendChild(newUL);
+            makeList(jsonObject[i],newUL);
+        }
+    }
+}
+
+
